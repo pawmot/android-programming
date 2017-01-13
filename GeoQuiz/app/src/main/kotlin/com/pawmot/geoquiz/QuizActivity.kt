dfb09764d73862e1e41a1e499077d5148839c6a5
Paweł Motyl <pawmot@gmail.com>
2017-01-13 23:25:07 +0100

    GEOQUIZ: Kotlinx and toasts

--- GeoQuiz/app/src/main/kotlin/com/pawmot/geoquiz/QuizActivity.kt
+++ GeoQuiz/app/src/main/kotlin/com/pawmot/geoquiz/QuizActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        trueButton.setOnClickListener { Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show() }
+        falseButton.setOnClickListener { Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
