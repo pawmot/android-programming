f68266e5916873879cfb8171341acf72bc247f2b
Paweł Motyl <pawmot@gmail.com>
2017-01-19 23:49:56 +0100

    CI: Add fragment with EditText

--- CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/CrimeActivity.kt
+++ CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/CrimeActivity.kt
@@ -1,12 +1,23 @@
 package com.pawmot.criminalintent
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 
-class CrimeActivity : AppCompatActivity() {
+class CrimeActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crime)
+
+        val fm = supportFragmentManager
+        var fragment: Fragment? = fm.findFragmentById(R.id.fragmentContainer)
+
+        if (fragment == null) {
+            fragment = CrimeFragment()
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit()
+        }
     }
 }
