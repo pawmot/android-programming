52ea1da6c485ceb03442e365357e9c5db56e3a78
Paweł Motyl <pawmot@gmail.com>
2017-01-19 23:01:19 +0100

    CI: Create the project

---
+++ CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/CrimeActivity.kt
@@ -0,0 +1,12 @@
+package com.pawmot.criminalintent
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CrimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crime)
+    }
+}
