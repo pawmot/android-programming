e34103001faca794552a4e1547a5507682b42af0
Paweł Motyl <pawmot@gmail.com>
2017-02-01 22:42:15 +0100

    CI Fragment Arguments

--- CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/CrimeFragment.kt
@@ -7,16 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pawmot.criminalintent.model.Crime
+import com.pawmot.criminalintent.model.CrimeLab
 import com.pawmot.criminalintent.util.TextWatcherChangeOnly
 import kotlinx.android.synthetic.main.fragment_crime.*
 import java.util.*
 
 class CrimeFragment : Fragment() {
+    companion object Companion {
+        private val argCrimeId = "crime_id"
+        fun newInstance(crimeId: UUID) : CrimeFragment {
+            val args = Bundle()
+            args.putSerializable(argCrimeId, crimeId)
+
+            val fragment = CrimeFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
     private lateinit var crime: Crime
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        crime = Crime.new()
+        val crimeId = arguments.getSerializable(argCrimeId) as UUID
+        crime = CrimeLab.instance(activity).getCrime(crimeId) ?: Crime.new()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,11 +38,13 @@ class CrimeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        crimeTitle.setText(crime.title)
         crimeTitle.addTextChangedListener(TextWatcherChangeOnly { crime.title = it })
 
         crimeDate.text = formatDate(crime.date)
         crimeDate.isEnabled = false
 
+        crimeSolved.isChecked = crime.solved
         crimeSolved.setOnCheckedChangeListener { compoundButton, checked -> crime.solved = checked }
     }
 
--- CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/ui/CrimeAdapter.kt
+++ CriminalIntent/app/src/main/kotlin/com/pawmot/criminalintent/ui/CrimeAdapter.kt
@@ -1,10 +1,11 @@
 package com.pawmot.criminalintent.ui
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pawmot.criminalintent.CrimeActivity
 import com.pawmot.criminalintent.R
 import com.pawmot.criminalintent.model.Crime
 import kotlinx.android.synthetic.main.list_item_crime.view.*
@@ -14,12 +15,12 @@ class CrimeAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<Crime
         val TAG = CrimeAdapter::class.simpleName
     }
 
-    class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    class CrimeHolder(view: View, private val ctx: Context) : RecyclerView.ViewHolder(view), View.OnClickListener {
         init {
             itemView.setOnClickListener(this)
         }
 
-        private var crime: Crime? = null
+        private lateinit var crime: Crime
 
         fun bindCrime(crime: Crime) {
             this.crime = crime
@@ -29,14 +30,15 @@ class CrimeAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<Crime
         }
 
         override fun onClick(v: View?) {
-            Log.i(TAG, "${crime?.title} clicked!")
+            val intent = CrimeActivity.newIntent(ctx, crime.uuid!!)
+            ctx.startActivity(intent)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent?.context)
         val view = inflater.inflate(R.layout.list_item_crime, parent, false)
-        return CrimeHolder(view)
+        return CrimeHolder(view, parent?.context!!)
     }
 
     override fun onBindViewHolder(holder: CrimeHolder?, position: Int) {
